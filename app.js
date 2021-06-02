1869ca698f9c8db49ee4977452c3aab66a480c88
Dmitry Volkov <hyardlung@gmail.com>
2021-06-03 01:02:49 +0700

    feat: настроить логирование запросов и ошибок
    Подключил модуль winston и express-winston, написал логгеры запросов и
    ошибок в файле logger.js, импортировал в app.js.
    Добавил обработчик ошибок celebrate.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/movexpdb' } = process.env;
 const app = express();
-
-app.use(express.json());
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
@@ -15,7 +15,11 @@ mongoose.connect(MONGO_URL, {
   useUnifiedTopology: true,
 });
 
-app.use(router);
-app.use(errorHandler);
+app.use(express.json());
+app.use(requestLogger); // логгер запросов
+app.use(router); // роуты
+app.use(errorLogger); // логгер ошибок
+app.use(errors()); // обработчик ошибок celebrate
+app.use(errorHandler); // централизованный обработчик ошибок
 
 app.listen(PORT);
