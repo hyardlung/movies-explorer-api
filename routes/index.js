9d72bf46a994aae9d3d9cd3a53cdb1171fdff7c1
Dmitry Volkov <hyardlung@gmail.com>
2021-06-06 20:23:31 +0700

    fix: защитить '*' авторизацией

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const movieRouter = require('./movies');
+const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/not-found-err');
 
 router.use(userRouter);
 router.use(movieRouter);
-router.use('*', () => {
+router.all('*', auth, () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 
